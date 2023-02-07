7631be1a3a2df8202bf2867f3b9a93c9aa5a7b79
Roman <kalinroman034@gmail.com>
2023-02-07 15:54:10 +0300

    Playlist maker v1.0

---
+++ app/src/main/java/com/practicum/playlistmaker/LibraryActivity.kt
@@ -0,0 +1,20 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+
+class LibraryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+        val goBackButton = findViewById<ImageButton>(R.id.goBack_button)
+
+        goBackButton.setOnClickListener {
+            val goBackIntent = Intent(this, MainActivity::class.java)
+            startActivity(goBackIntent)
+        }
+    }
+}
